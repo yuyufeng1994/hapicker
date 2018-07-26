43ceed0b5d04878c757947f8b0e121f7ecac80b3
yuyufeng <416530244@qq.com>
2018-07-26 16:50:12 +0800

    commit

--- hapicker-web/src/main/java/com/hapicker/web/action/IndexAction.java
+++ hapicker-web/src/main/java/com/hapicker/web/action/IndexAction.java
@@ -2,6 +2,7 @@ package com.hapicker.web.action;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @date 2018/7/26.
  */
 @Controller
+@RefreshScope
 public class IndexAction {
-    @Value("${param1}")
+    @Value("${base.name}")
     private String param;
 
     @RequestMapping(value = "index",method = RequestMethod.GET)
