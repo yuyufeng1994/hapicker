b38fb9a67f054d521567d7ee8c7edc908444df3c
yuyufeng <416530244@qq.com>
2018-07-27 16:32:56 +0800

    commit

--- hapicker-web/src/main/java/com/hapicker/web/action/IndexAction.java
+++ hapicker-web/src/main/java/com/hapicker/web/action/IndexAction.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author yuyufeng
@@ -17,9 +18,22 @@ public class IndexAction {
     @Value("${base.name}")
     private String param;
 
-    @RequestMapping(value = "index",method = RequestMethod.GET)
-    String index(){
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    String index() {
         System.out.println(param);
         return "index";
+    }
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    String home() {
+        System.out.println(param);
+        return "homepage";
+    }
+
+
+    @RequestMapping(value = "json", method = RequestMethod.GET)
+    @ResponseBody
+    String json() {
+        return "index首页";
     }
 }
