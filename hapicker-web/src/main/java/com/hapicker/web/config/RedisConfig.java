fc0744935afdb124cac9b23241ead51aa3b1f06a
yuyufeng <416530244@qq.com>
2018-08-21 17:26:36 +0800

    commit

--- hapicker-web/src/main/java/com/hapicker/web/config/RedisConfig.java
+++ hapicker-web/src/main/java/com/hapicker/web/config/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -16,11 +15,9 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import redis.clients.jedis.JedisPoolConfig;
-
-import java.lang.reflect.Method;
-
 /**
  * @author yuyufeng
+ * @date 2018年8月21日17:25:57
  */
 @Configuration
 @EnableCaching
@@ -67,7 +64,6 @@ public class RedisConfig extends CachingConfigurerSupport {
     }
     @Bean
     public JedisPoolConfig poolCofig() {
-
         JedisPoolConfig poolCofig = new JedisPoolConfig();
         poolCofig.setMaxIdle(properties.getMaxIdle());
         poolCofig.setMaxTotal(properties.getMaxTotal());
