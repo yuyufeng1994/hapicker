03b53300df8e55aa455398a0663a00cb5a52cae2
yuyufeng <416530244@qq.com>
2018-12-06 19:28:03 +0800

    commit

---
+++ hapicker-common/src/main/java/com/hapicker/common/constant/ExceptionCode.java
@@ -0,0 +1,15 @@
+package com.hapicker.common.constant;
+
+/**
+ * @author yuyufeng
+ * @date 2018/12/6.
+ */
+public class ExceptionCode {
+
+    //没有权限
+    public final static String NO_PERMISSION = "505";
+    //没有登录
+    public final static String NO_LOGIN = "506";
+
+
+}
